06960fd5b333cbdbe5562e77a9de67bb2c5d7743
Yixiao Zeng <yixiaozeng0208@outlook.com>
2024-05-03 10:55:09 -0700

    select 600 leetcode problems for testing

---
+++ select_leetcode_problems.py
@@ -0,0 +1,16 @@
+import random
+import csv
+from datasets import load_dataset
+
+dataset = load_dataset("greengerong/leetcode")
+
+problem_data = dataset["train"]
+problem_ids = problem_data["id"]
+problem_slugs = problem_data["slug"]
+
+selected_indices = random.sample(range(len(problem_ids)), min(600, len(problem_ids)))
+selected_problems = [(problem_ids[i], problem_slugs[i]) for i in selected_indices]
+csv_filename = 'selected_leetcode_problems.csv'
+with open(csv_filename, 'w', newline='') as file:
+    writer = csv.writer(file)
+    writer.writerows(selected_problems)  # Write the selected problem IDs and slugs
